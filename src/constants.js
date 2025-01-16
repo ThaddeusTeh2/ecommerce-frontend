a808a64f6023fc40f68c90ebc74351f524e846e9
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2025-01-16 15:01:24 +0800

    pain 7

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5555/api"
-    : "http://b13-thaddeus24.mak3r.dev/api";
+    : "https://b13-thaddeus24.mak3r.dev/api";
